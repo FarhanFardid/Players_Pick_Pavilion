c0d454fae269923855fda1f2125824c5cc44402c
FarhanFardid <farhanfardid009@gmail.com>
2024-03-05 22:38:53 +0600

    Json data updated and player display in the players componenet

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,23 @@
+
+
+const Player = (props) => {
+    const player = props.player; 
+    const {id,player_name,image,role,price,country} = player;
+
+    return (
+       
+<div  className="flex flex-col items-center  bg-gray-300 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-full hover:bg-gray-400 ">
+    <div className="w-32 h-32 lg:w-52 lg:h-52">    <img className="object-cover w-full h-full bg-slate-100 rounded-lg p-1 lg:p-2" src={image} alt="player image"/></div>
+
+    <div className="flex flex-col justify-center items-center p-1 lg:p-2 text-center mx-auto">
+        <h5 className="mb-2 text-lg lg:text-2xl font-bold tracking-tight text-green-800 ">{player_name}</h5>
+        <p className="mb-3 font-semibold text-sm lg:text-lg text-red-700 ">Role: {role}</p>
+        <p className="mb-3 font-semibold text-sm lg:text-lg  text-blue-600 ">Price: {price}</p>
+        <p className="mb-3 font-semibold text-sm lg:text-lg text-green-950 ">Country: {country}</p>
+    </div>
+</div>
+        
+    );
+};
+
+export default Player;
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Player from "../Player/Player";
 
 const Players = () => {
-    
+
+    const [players, setPlayers] = useState([]);
+    useEffect(()=>{
+        fetch("data.json")
+        .then(res => res.json())
+        .then (data => setPlayers(data.players))
+    },[])
+
     return (
-        <div>
-            <h3 className="text-xl lg:text-4xl font-bold text-slate-900 p-2 lg:p-4  text-center">Players Profile</h3>
+        <div className="bg-slate-500">
+            <h3 className="text-xl lg:text-4xl font-bold text-blue-800 p-2 lg:p-4  text-center">Players Profile</h3>
+            <p className="text-center text-sm lg:text-2xl font-semibold my-1 lg:my-5 text-white">Total Number of Available Players: {players.length}</p>
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-2">
+                {
+                    players.map(player => <Player key={player.id} player={player}> </Player>)
+                }
+            </div>
         </div>
     );
 };
