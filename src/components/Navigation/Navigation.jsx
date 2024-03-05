9d706858703fcadb92aba1eec30d00ccb7246921
FarhanFardid <farhanfardid009@gmail.com>
2024-03-05 22:54:30 +0600

    players container component and player component complete

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,8 +10,8 @@ const Navigation = () => {
   <div  className="flex items-center justify-around lg:p-4 p-1">
       <img src={logo} className="h-10 w-10 lg:h-24 lg:w-24 rounded-full m-1 lg:m-3" alt="Player Pick Logo"/>
       <div className='flex flex-col items-center justify-center' >
-        <h3 className="self-center text-xl lg:text-5xl font-bold whitespace-nowrap text-blue-900 py-1 lg:py-2">Players Pick Pavilion</h3> 
-        <p className=' text-sm lg:text-lg text-red-800 font-semibold py-1 lg:py-2' >Pick Your Favorite Players & Make Your Dream Fantasy Team</p></div>
+        <h3 className="self-center text-xl lg:text-5xl font-bold whitespace-nowrap text-slate-950 py-1 lg:py-2">Players Pick Pavilion</h3> 
+        <p className=' text-sm lg:text-lg text-red-800 font-semibold py-1 lg:py-2' >Pick Your Favorite Players & Make Your Dream 11 Fantasy Team</p></div>
       <img src={logo} className="h-10 w-10 lg:h-24 lg:w-24 rounded-full m-1 lg:m-3" alt="Player Pick Logo"/>
   </div>
   
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -14,6 +14,7 @@ const Player = (props) => {
         <p className="mb-3 font-semibold text-sm lg:text-lg text-red-700 ">Role: {role}</p>
         <p className="mb-3 font-semibold text-sm lg:text-lg  text-blue-600 ">Price: {price}</p>
         <p className="mb-3 font-semibold text-sm lg:text-lg text-green-950 ">Country: {country}</p>
+        <button onClick={()=>{console.log(id)} } className="border-2 border-black rounded-lg py-1 px-2 font-bold text-sm bg-slate-800 text-white hover:bg-slate-950 ">Pick for Dream11</button>
     </div>
 </div>
         
